7b28f069af0de9e6f9b7d876088f1151f11115e5
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-20 00:41:38 -0300

    correccion en lista tareas

--- src/ListaTareas.jsx
+++ src/ListaTareas.jsx
@@ -3,7 +3,7 @@ import { Tarea } from "./Tarea";
 export const ListaTareas = ({ tareas, eliminar }) => {
   return (
     <>
-      {tareas.length > 0 ? (
+      {tareas?.length > 0 ? (
         <ul className="list-unstyled">
           {tareas?.map((tarea, i) => (
             <Tarea tarea={tarea} key={i} eliminar={eliminar} tareas={tareas}/>
